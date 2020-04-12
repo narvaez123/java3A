e95bafe152df0bf342912960e5b63ccfbd6810bf
johan <sebastiannnar021@gmail.com>
2020-04-12 22:03:25 +0200

    cadenas1

--- practicas/src/Cadenas1.java
+++ practicas/src/Cadenas1.java
@@ -8,12 +8,12 @@
  *
  * @author JOHAN
  */
-public class StringManager extends javax.swing.JFrame {
+public class Cadenas1 extends javax.swing.JFrame {
     
     /**
      * Creates new form StringManager 
      */
-    public StringManager() {
+    public Cadenas1() {
         initComponents();
         
         jButton1.setEnabled(false); 
@@ -515,20 +515,21 @@ public class StringManager extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(StringManager.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Cadenas1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new StringManager().setVisible(true);
+                new Cadenas1().setVisible(true);
             }
         });
     }
